11c15d87876b5881c2eace19d0fc62110b67d0e3
Ovidiojcf <ojcfarias@gmail.com>
2025-03-19 15:45:46 -0300

    first commit

---
+++ src/components/HeaderComponent.vue
@@ -0,0 +1,22 @@
+<script>
+import BaseIcon from './BaseIcon.vue';
+</script>
+
+<template>
+    <header class="bg-white h-32 flex flex-row ">
+        <div class="basis-3xs">
+            <img src="@/assets/cyber-logo-black.svg" alt="">
+        </div>
+        <div class="basis-3xs">
+            <p>Barra de navegação</p>
+        </div>
+        <div class="basis-3xs">
+            <p>links de navegação</p>
+        </div>
+        <div class="basis-3xs">
+            <BaseIcon category="actions" name="Favorites" size="24px" color="red" />
+            <BaseIcon category="actions" name="Favorites" size="24px" color="red" />
+            <BaseIcon category="actions" name="Favorites" size="24px" color="red" />
+        </div>
+    </header>
+</template>
---
+++ src/views/HomeView.vue
@@ -0,0 +1,9 @@
+<script setup>
+import Header from '@/components/HeaderComponent.vue'
+</script>
+
+<template>
+  <Header></Header>
+  <main>
+  </main>
+</template>
