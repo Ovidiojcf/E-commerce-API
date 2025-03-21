cdc96d42a50eac7bba5e7018c8fb8c5b343962e7
Ovidiojcf <ojcfarias@gmail.com>
2025-03-21 13:25:49 -0300

    style: update linear from HeroComponent

--- src/components/HeroComponent.vue
+++ src/components/HeroComponent.vue
@@ -2,9 +2,9 @@
     <section class="w-full h-full custom-gradient flex flex-row justify-center px-10 gap-x-70">
         <div class="flex flex-col justify-center gap-1">
             <h3 class="text-3xl !font-semibold text-white">Pro.Beyond.</h3>
-            <h1 class="text-9xl text-white">IPhone 14 <strong class="text-violet-800">Pro</strong></h1>
+            <h1 class="text-9xl text-white tracking-tight">IPhone 14 <strong class="text-violet-800">Pro</strong></h1>
             <p class="text-2xl text-white py-2">Created to change everything for the better. For everyone</p>
-            <button class="w-fit text-white px-7.5 py-2.5 border-4 border-gray-700 rounded-xl hover:bg-violet-800 text-black">Shop Now</button>
+            <button class="w-fit text-white px-7.5 py-2.5 border-4 border-black rounded-xl hover:bg-violet-800 text-black">Shop Now</button>
         </div>
         <div>
             <img src="@/assets/iphoneImage.svg" alt="">
@@ -15,15 +15,14 @@
 <style>
 .custom-gradient {
   background: linear-gradient(
-    to bottom,
-    #FFFFFF 0%,
-    #C0C0C0 10%,
-    #C0C0C0 30%,
-    #C0C0C0 30%,
-    #A0A0A0 45%,
-    #808080 60%,
-    #909090 75%,
-    #909090 100%
+    to top,
+    #8e8e8e 30%,
+    #8f8f8f 30%,
+    #707070 45%,
+    #707070 50%,
+    #707070 60%,
+    #5f5f5f 75%,
+    #2c2c2c 100%
   );
 }
 </style>
