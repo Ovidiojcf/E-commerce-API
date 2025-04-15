9ec804e7f7024cf92b5d65dbea551d4884ddba2f
Ovidiojcf <ojcfarias@gmail.com>
2025-04-15 13:50:30 -0300

    update div increment/decrement to use flex

--- src/components/CartItemComponent.vue
+++ src/components/CartItemComponent.vue
@@ -22,21 +22,24 @@ const totalPrice = computed(() => {
             <img class="rounded-xl" :src="item.image" :alt="item.name">
         </div>
         <div class="flex items-center p-4">
-            <h3 class="!font-bold text-black text-start">{{ item.name }}</h3>
-            <span class="text-gray-500 text-sm">R$ {{ (item.unit_price ?? 0).toFixed(2) }}</span>
+            <span class="!font-bold text-black">UN R$ {{ (item.unit_price ?? 0).toFixed(2) }}</span>
         </div>
         <div class="flex items-center gap-8">
-            <div class="grid grid-cols-3">
+            <div class="flex">
                 <button @click="$emit('decrement', item)"
                     class="w-8 h-8 flex items-center justify-center rounded-l-md border border-gray-300 bg-gray-100 text-gray-600 hover:bg-gray-200 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2">
-                    - </button>
+                    -
+                </button>
                 <div
                     class="w-12 h-8 flex items-center justify-center border-t border-b border-gray-300 bg-white text-gray-900">
-                    {{ item.quantity }}</div>
+                    {{ item.quantity }}
+                </div>
                 <button @click="$emit('increment', item)"
                     class="w-8 h-8 flex items-center justify-center rounded-r-md border border-gray-300 bg-gray-100 text-gray-600 hover:bg-gray-200 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2">
-                    + </button>
+                    +
+                </button>
             </div>
+
             <div class="pl-5">
                 <h2 class="!font-bold">R$ {{ totalPrice }}</h2>
             </div>
