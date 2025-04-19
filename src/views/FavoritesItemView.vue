470021f8af0720961e063ab6f303ffa3b6d3b0c2
Ovidiojcf <ojcfarias@gmail.com>
2025-04-19 19:53:04 -0300

    feat(favorites): add toggle functionality with persistent localStorage state

--- src/views/FavoritesItemView.vue
+++ src/views/FavoritesItemView.vue
@@ -1,11 +1,30 @@
 <script setup>
-import Header from '@/components/HeaderComponent.vue';
+import Header from '@/components/HeaderComponent.vue'
+import ProductComponent from '@/components/ProductComponent.vue'
+import { useFavoritesStore } from '@/stores/favorites'
+import { computed } from 'vue';
+
+const favoritesStore = useFavoritesStore()
+const favorites = computed(() => favoritesStore.favorites)
+
 </script>
 
 <template>
-    <Header/>
-    <div class="favorites-item-view">
-        <h1>Favorites Item View</h1>
-        <p>This is the favorites item view.</p>
+    <Header />
+
+    <div class="container mx-auto px-4 py-8">
+        <h1 class="text-2xl font-bold mb-6">Meus Favoritos</h1>
+
+        <div v-if="favorites.length" class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-6">
+            <div v-for="product in favorites" :key="product.id" class="relative">
+                <!-- Reuso do ProductComponent -->
+                <ProductComponent :id="product.id" :name="product.name" :price="product.price"
+                    :imageUrl="product.imageUrl" />
+            </div>
+        </div>
+
+        <p v-else class="text-center text-gray-500">
+            Nenhum produto favoritado ainda.
+        </p>
     </div>
 </template>
