0458a17a8283899f621194b29ad866e1c1f42140
Ovidiojcf <ojcfarias@gmail.com>
2025-04-24 17:33:54 -0300

    style: minor updates to UserProfileCard layout and design

--- src/components/UserProfileCard.vue
+++ src/components/UserProfileCard.vue
@@ -18,7 +18,7 @@ function getImg(imagePath) {
     <div
         class="flex flex-row w-full max-w-sm bg-white dark:bg-gray-800 rounded-xl shadow-md overflow-hidden gap-6 p-6 text-center">
         <img :src="getImg(avatarUrl)" alt="Foto do usuário"
-            class="w-24 h-24 rounded-full mx-auto mb-4 object-cover border-2 border-gray-300" />
+            class="w-30 h-30 rounded-full mx-auto mb-4 object-cover border-2 border-gray-300" />
         <div class="flex flex-col justify-center items-start">
             <h2 class="text-xl !font-semibold text-gray-800 dark:text-white">{{ name }}</h2>
             <p class="text-gray-600 dark:text-gray-300">{{ email }}</p>
