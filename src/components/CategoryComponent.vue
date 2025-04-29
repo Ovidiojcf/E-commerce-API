64fbda92066ae7ebf477f0207daf6f5e5e67162d
Ovidiojcf <ojcfarias@gmail.com>
2025-04-29 20:49:33 -0300

    fix(category): standardize category titles and logs to English

--- src/components/CategoryComponent.vue
+++ src/components/CategoryComponent.vue
@@ -18,26 +18,26 @@ function getImgCategory(image_path) {
 onMounted(async () => {
     await store.getCategoriesStore();
     allCategories.value = store.categories;
-    console.log("Resposta da API para Categories: ", allCategories);
+    console.log("API response for Categories: ", allCategories);
 })
 </script>
 
 
 <template>
     <section class="w-full px-10">
-        <h2 class="text-xl font-bold !mt-4 !mb-4">Ofertas por categoria</h2>
+        <h2 class="text-xl font-bold !mt-4 !mb-4">Offers by Category</h2>
 
         <Swiper :modules="[Navigation]" :breakpoints="{
-            320: { slidesPerView: 2.5, spaceBetween: 25 },
-            640: { slidesPerView: 3.5, spaceBetween: 15 },
-            1024: { slidesPerView: 4, spaceBetween: 15 }
+            320: { slidesPerView: 2.5, spaceBetween: 0 },
+            640: { slidesPerView: 3.5, spaceBetween: 0 },
+            1024: { slidesPerView: 4, spaceBetween: 0 }
         }" navigation class="w-full">
             <SwiperSlide v-for="(item, index) in allCategories" :key="index"
-                class="flex flex-col items-center justify-center text-center">
-                <div class="w-40 h-40 rounded-full flex items-center justify-center overflow-hidden !mb-2">
-                    <img :src="getImgCategory(item.image_path)" :alt="item.name" class="object-cover" />
+                class="!flex !flex-col !items-center !justify-center text-center">
+                <div class="w-40 h-40 rounded-full flex items-center justify-center overflow-hidden !mb-2 bg-gray-100">
+                    <img :src="getImgCategory(item.image_path)" :alt="item.name" class="object-cover w-full h-full" />
                 </div>
-                <p class="w-full text-center text-sm !font-bold">{{ item.name }}</p>
+                <p class="text-sm !font-bold mt-2">{{ item.name }}</p>
             </SwiperSlide>
         </Swiper>
     </section>
