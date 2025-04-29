c09874587000d8fd038cccd956b672f59c4fa50a
Ovidiojcf <ojcfarias@gmail.com>
2025-04-29 13:27:39 -0300

    feat(category): implement Swiper carousel with responsive layout and centered items

--- src/components/CategoryComponent.vue
+++ src/components/CategoryComponent.vue
@@ -10,10 +10,10 @@ import { Navigation } from 'swiper/modules';
 const allCategories = ref([]);
 const store = useGetCategories();
 
-function getImgCategory(image_path){
-    const baseUrl = 'http://35.196.79.227:8000/' //base url da api
+function getImgCategory(image_path) {
+    const baseUrl = 'http://35.196.79.227:8000' //base url da api
     return `${baseUrl}${image_path}`
-    }
+}
 
 onMounted(async () => {
     await store.getCategoriesStore();
@@ -24,30 +24,21 @@ onMounted(async () => {
 
 
 <template>
-    <section class="w-full px-4">
-      <h2 class="text-xl font-bold mb-4">Ofertas por categoria</h2>
-  
-      <Swiper
-        :modules="[Navigation]"
-        :breakpoints="{
-          320: { slidesPerView: 2.5, spaceBetween: 10 },
-          640: { slidesPerView: 3.5, spaceBetween: 15 },
-          1024: { slidesPerView: 5, spaceBetween: 20 }
-        }"
-        navigation
-        class="w-full"
-      >
-        <SwiperSlide
-            v-for="(item, index) in allCategories"
-          :key="index"
-          class="flex flex-col items-center"
-        >
-          <div class="bg-indigo-600 rounded-full p-4 w-24 h-24 flex items-center justify-center">
-            <img :src="getImgCategory(item.image_path)" :alt="item.name" class="w-16 h-16 object-contain" />
-          </div>
-          <p class="mt-2 text-center text-sm font-medium">{{ item.name }}</p>
-        </SwiperSlide>
-      </Swiper>
+    <section class="w-full px-10">
+        <h2 class="text-xl font-bold !mt-4 !mb-4">Ofertas por categoria</h2>
+
+        <Swiper :modules="[Navigation]" :breakpoints="{
+            320: { slidesPerView: 2.5, spaceBetween: 25 },
+            640: { slidesPerView: 3.5, spaceBetween: 15 },
+            1024: { slidesPerView: 4, spaceBetween: 15 }
+        }" navigation class="w-full">
+            <SwiperSlide v-for="(item, index) in allCategories" :key="index"
+                class="flex flex-col items-center justify-center text-center">
+                <div class="w-40 h-40 rounded-full flex items-center justify-center overflow-hidden !mb-2">
+                    <img :src="getImgCategory(item.image_path)" :alt="item.name" class="object-cover" />
+                </div>
+                <p class="w-full text-center text-sm !font-bold">{{ item.name }}</p>
+            </SwiperSlide>
+        </Swiper>
     </section>
-  </template>
-  
+</template>
