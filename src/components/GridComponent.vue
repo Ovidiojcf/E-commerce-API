65e9a2843bca3ed05c6a6dd9fedfc0ec18ff5b6f
Ovidiojcf <ojcfarias@gmail.com>
2025-04-29 20:50:20 -0300

    feat(grid): standardize grid component text to English

--- src/components/GridComponent.vue
+++ src/components/GridComponent.vue
@@ -5,13 +5,13 @@
         <div class="bg-purple-700 grid grid-cols-1 sm:grid-cols-2 items-center gap-4 rounded-lg">
             <img class="w-full p-6" src="@/assets/playstation5.svg" alt="PlayStation 5" />
             <div class="flex flex-col justify-center gap-4 p-3">
-                <h2 class="text-3xl sm:text-5xl text-center font-bold text-black">PlayStation 5</h2>
+                <h2 class="text-3xl sm:text-5xl text-center font-bold text-white">PlayStation 5</h2>
                 <!-- <p class="text-base sm:text-xl font-semibold text-justify">
                         Experience the incredible possibilities of gaming that go beyond the extraordinary on
                         PlayStation 5 and PS5 Digital Edition consoles.
                     </p> -->
                 <button
-                    class="self-center px-6 py-2 border-2 border-black font-bold rounded-xl hover:bg-blue-900 hover:text-white transition-all">
+                    class="self-center px-6 py-2 border-2 border-black font-bold rounded-xl hover:bg-white hover:text-purple-700 transition-all">
                     Shop Now
                 </button>
             </div>
